ec6e57f785d8397bafac5157013a630cf3c13a9b
xexex98 <xexex98@gmail.com>
2024-06-10 01:10:59 +0300

    Add ufo

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,23 +3,7 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'kevinhwang91/nvim-ufo',
-    event = 'VeryLazy',
-    dependencies = { 'kevinhwang91/promise-async' },
-    config = function()
-      require('ufo').setup {}
-      vim.o.foldcolumn = '0' -- '0' is not bad
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-
-      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-    end,
-  },
-
+  require 'custom.plugins.ufo',
   require 'custom.plugins.trouble',
   require 'custom.plugins.matchup',
   require 'custom.plugins.project',
