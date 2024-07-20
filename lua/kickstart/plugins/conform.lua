47cfaa9f5ca8c5aab10a87050a9f638408489a7d
xexex98 <xexex98@gmail.com>
2024-07-21 01:41:41 +0300

    remove autoformat on save in conform config

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -7,7 +7,7 @@ return {
     {
       '<leader>f',
       function()
-        require('conform').format { async = true, lsp_format = 'first' }
+        require('conform').format { async = true, lsp_format = 'prefer' }
       end,
       mode = '',
       desc = '[F]ormat buffer',
@@ -15,11 +15,6 @@ return {
   },
   opts = {
     notify_on_error = true,
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_format = 'fallback',
-    },
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
