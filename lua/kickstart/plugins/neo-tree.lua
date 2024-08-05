29cf70b5ea1f03ad1171f760a66df2f6299a701a
xexex98 <xexex98@gmail.com>
2024-08-05 09:08:56 +0300

    use nvim-tree instead neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,39 +1,90 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5 -- You can change this too
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
+  'nvim-tree/nvim-tree.lua',
   version = '*',
+  lazy = false,
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+    'nvim-tree/nvim-web-devicons',
   },
-  cmd = 'Neotree',
+  config = function()
+    require('nvim-tree').setup {
+      disable_netrw = true,
+      hijack_netrw = true,
+      respect_buf_cwd = true,
+      sync_root_with_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+      view = {
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = 'rounded',
+              relative = 'editor',
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+        end,
+      },
+    }
+  end,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = false,
-        show_hidden_count = false,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          '.git',
-          '.DS_Store',
-          'build',
-          'node_modules',
-        },
-        never_show = {},
-      },
-      window = {
-        position = 'float',
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
+    { '\\', ':NvimTreeToggle<CR>', desc = 'Toggle NvimTree' },
   },
 }
+-- return {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   version = '*',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+--     'MunifTanjim/nui.nvim',
+--   },
+--   cmd = 'Neotree',
+--   keys = {
+--     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+--   },
+--   opts = {
+--     filesystem = {
+--       filtered_items = {
+--         visible = false,
+--         show_hidden_count = false,
+--         hide_dotfiles = false,
+--         hide_gitignored = true,
+--         hide_by_name = {
+--           '.git',
+--           '.DS_Store',
+--           'build',
+--           'node_modules',
+--         },
+--         never_show = {},
+--       },
+--       window = {
+--         position = 'float',
+--         mappings = {
+--           ['\\'] = 'close_window',
+--         },
+--       },
+--     },
+--   },
+-- }
