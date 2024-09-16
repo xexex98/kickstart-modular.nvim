9d6240b70d1d5d4c1e0fd386d357a4b40cde4ddf
xexex98 <xexex98@gmail.com>
2024-09-16 22:57:33 +0300

    change neotree to nvimtree

--- lua/kickstart/plugins/nvim-tree.lua
+++ lua/kickstart/plugins/nvim-tree.lua
@@ -52,39 +52,3 @@ return {
     { '\\', ':NvimTreeToggle<CR>', desc = 'Toggle NvimTree', silent = true },
   },
 }
--- return {
---   'nvim-neo-tree/neo-tree.nvim',
---   version = '*',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
---     'MunifTanjim/nui.nvim',
---   },
---   cmd = 'Neotree',
---   keys = {
---     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
---   },
---   opts = {
---     filesystem = {
---       filtered_items = {
---         visible = false,
---         show_hidden_count = false,
---         hide_dotfiles = false,
---         hide_gitignored = true,
---         hide_by_name = {
---           '.git',
---           '.DS_Store',
---           'build',
---           'node_modules',
---         },
---         never_show = {},
---       },
---       window = {
---         position = 'float',
---         mappings = {
---           ['\\'] = 'close_window',
---         },
---       },
---     },
---   },
--- }
